383b9e59c525975edf793011901eaab8f2721dfe
Alucard <yuri.corvino29@gmail.com>
2018-06-29 15:56:20 +0200

    fix

--- execute.php
+++ execute.php
@@ -1,10 +1,12 @@
 <?php
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
+
 if(!$update)
 {
   exit;
 }
+
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -13,26 +15,11 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
+
 $text = trim($text);
 $text = strtolower($text);
+
 header("Content-Type: application/json");
-$response = '';
-if(strpos($text, "/start") === 0 || $text=="ciao")
-{
-	$response = "Ciao $firstname, benvenuto!";
-}
-elseif($text=="domanda 1")
-{
-	$response = "risposta 1";
-}
-elseif($text=="domanda 2")
-{
-	$response = "risposta 2";
-}
-else
-{
-	$response = "Comando non valido!";
-}
-$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters = array('chat_id' => $chatId, "text" => $text);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
